97fec225452f771991c5779242c06e5aaaed35e2
unknown <louiecervantes@wvsu.edu.ph>
2023-05-16 09:11:39 +0800

    save changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,10 +69,11 @@ def app():
     st.write('Next steps:')
     st.write('Download the CSV file and upload to your Google Drive')
     st.write('Open the file using Google Sheets and check that it is formatted correctly. See image below.')
-    st.write('Make sure the columns are as follows: Question, Option A, Option B, Option C, Option D, Answer. Add a column ad the end "Points". Put the points for each item. See image below.')
+    st.write('Make sure the columns are as follows: Question, Option A, Option B, Option C, Option D, Answer. Add a column at the end and name it "Points". Put the point for each item. See image below.')
     st.write('By using "Form Builder for Sheets", you can save time by directly creating a Google Form from your Google Sheet exam questions, without having to manually enter them into the form.')
     st.write('Ask chatGPT for the steps on how to convert a GSheets exam to Google Form.')
     st.write('Click on the link below to try a Google Form exam created using the precedure described above.')
+    st.markdown("[Sample Weebsu-Quiz](https://forms.gle/Kkja7C9M8A1r8BDBA)")
     st.write('\n\n\nDisclaimer: Weebsu may produce inconsistent file format.')
     st.write('\n\n\n© 2023 West Visayas State University - Management Information System Office.')
 
